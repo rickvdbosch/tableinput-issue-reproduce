e425145345dcfb93ad40853a8e4ef689a6dc1abc
Rick van den Bosch <rickvdbosch@outlook.com>
2024-10-09 11:27:16 +0200

    TableEntity to the rescue?

---
+++ GetTableEntityWorks.cs
@@ -0,0 +1,24 @@
+using Azure.Data.Tables;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+using TableInputIssue.Entities;
+using TableInputIssue.Helpers;
+
+namespace TableInputIssue
+{
+    public class GetTableEntityWorks(ILogger<GetTableEntityWorks> logger)
+    {
+        [Function(nameof(GetTableEntityWorks))]
+        public IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "tableentity/{partitionKey}/{rowKey}")] HttpRequest req,
+            [TableInput("AwesomeEntities", "{partitionKey}", "{rowKey}")] TableEntity entity, string partitionKey)
+        {
+            logger.LogInformation("GetTableEntityWorks called");
+            string start = StartHelper.GetStart(partitionKey);
+            return new OkObjectResult($"{start} {entity[nameof(AwesomeEntity.WhatMakesThisSpecial)]}!");
+        }
+    }
+}
