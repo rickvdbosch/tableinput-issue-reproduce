fabbce37fa3e2ad842a86fac10c45a77f72707a5
Rick van den Bosch <rickvdbosch@outlook.com>
2024-10-09 11:26:33 +0200

    Implemented helper

---
+++ Helpers/StartHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace TableInputIssue.Helpers;
+
+public static class StartHelper
+{
+    public static string GetStart(string partitionKey) => partitionKey switch
+    {
+        Constants.HERE_BE_PARTITIONKEY => Constants.HERE_BE_START,
+        Constants.YOU_SHALL_PARTITIONKEY => Constants.YOU_SHALL_START,
+        _ => Constants.UNKNOWN,
+    };
+}
