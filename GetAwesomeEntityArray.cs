817a9b55820dc87bfda3fcf1df2abfa50cb516ab
Rick van den Bosch <rickvdbosch@outlook.com>
2024-10-09 11:27:08 +0200

    Getting an Array works

---
+++ GetAwesomeEntityArray.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+using TableInputIssue.Entities;
+
+namespace TableInputIssue
+{
+    public class GetAwesomeEntityArray(ILogger<GetAwesomeEntityArray> logger)
+    {
+        [Function(nameof(GetAwesomeEntityArray))]
+        public IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "awesomeentityarray/{partitionKey}/{rowKey}")] HttpRequest req,
+            [TableInput("AwesomeEntities", "{partitionKey}", "{rowKey}")] IEnumerable<AwesomeEntity> entities)
+        {
+            logger.LogInformation("GetAwesomeEntityArray called");
+            return new OkObjectResult(entities);
+        }
+    }
+}
