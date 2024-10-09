ed573cdf3e54643d5205918a8d283e4ffa06f421
Rick van den Bosch <rickvdbosch@outlook.com>
2024-10-09 11:26:40 +0200

    Added Constants

---
+++ Constants/Constants.cs
@@ -0,0 +1,10 @@
+﻿public static class Constants
+{
+    public const string HERE_BE_PARTITIONKEY = "here-be";
+    public const string YOU_SHALL_PARTITIONKEY = "you-shall";
+
+    public const string HERE_BE_START = "Here be";
+    public const string YOU_SHALL_START = "You shall not";
+
+    public const string UNKNOWN = "Unknown";
+}
